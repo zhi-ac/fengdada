ac2e1c02d1b7d84f5c583ae7626db20f39ca50a4
zhi-ac <1252580622@qq.com>
2024-12-18 21:29:35 +0800

    后端使用模板初始化

---
+++ fengdada-backend/src/main/java/com/sheng/fengdada/controller/FileController.java
@@ -0,0 +1,63 @@
+package com.sheng.fengdada.controller;
+
+import cn.hutool.core.io.FileUtil;
+import com.sheng.fengdada.common.BaseResponse;
+import com.sheng.fengdada.common.ErrorCode;
+import com.sheng.fengdada.common.ResultUtils;
+import com.sheng.fengdada.constant.FileConstant;
+import com.sheng.fengdada.exception.BusinessException;
+import com.sheng.fengdada.model.dto.file.UploadFileRequest;
+import com.sheng.fengdada.model.entity.User;
+import com.sheng.fengdada.model.enums.FileUploadBizEnum;
+import com.sheng.fengdada.service.UserService;
+
+import java.io.File;
+import java.util.Arrays;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 文件接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/file")
+@Slf4j
+public class FileController {
+
+    @Resource
+    private UserService userService;
+
+
+
+    /**
+     * 校验文件
+     *
+     * @param multipartFile
+     * @param fileUploadBizEnum 业务类型
+     */
+    private void validFile(MultipartFile multipartFile, FileUploadBizEnum fileUploadBizEnum) {
+        // 文件大小
+        long fileSize = multipartFile.getSize();
+        // 文件后缀
+        String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
+        final long ONE_M = 1024 * 1024L;
+        if (FileUploadBizEnum.USER_AVATAR.equals(fileUploadBizEnum)) {
+            if (fileSize > ONE_M) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "文件大小不能超过 1M");
+            }
+            if (!Arrays.asList("jpeg", "jpg", "svg", "png", "webp").contains(fileSuffix)) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "文件类型错误");
+            }
+        }
+    }
+}
