072108c63123e2e6c1e28e973bedcc8aa7b4445a
zhi-ac <1252580622@qq.com>
2024-12-28 18:44:17 +0800

    开发前端用户登录、注册、管理页面

--- fengdada-frontend/src/router/index.ts
+++ fengdada-frontend/src/router/index.ts
@@ -1,40 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import UserLayout from "@/layouts/UserLayout.vue";
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/user/login",
-    name: "用户",
-    component: UserLayout,
-    // children: [
-    //   {
-    //     path: "/user/login",
-    //     name: "用户登录",
-    //     component: UserLoginView,
-    //   },
-    //   {
-    //     path: "/user/register",
-    //     name: "用户注册",
-    //     component: UserRegisterView,
-    //   },
-    // ],
-  },
-];
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "@/router/routes";
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
--- fengdada-frontend/src/router/routes.ts
+++ fengdada-frontend/src/router/routes.ts
@@ -2,6 +2,10 @@ import { RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import NoAuthPage from "@/views/NoAuthPage.vue";
 import ACCESS_ENUM from "@/access/accessEnum";
+import UserRegisterPage from "@/views/user/UserRegisterPage.vue";
+import UserLoginPage from "@/views/user/UserLoginPage.vue";
+import UserLayout from "@/layouts/UserLayout.vue";
+import AdminUserPage from "@/views/admin/AdminUserPage.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
@@ -9,6 +13,35 @@ export const routes: Array<RouteRecordRaw> = [
     name: "home",
     component: HomeView,
   },
+  {
+    path: "/user",
+    name: "用户",
+    component: UserLayout,
+    children: [
+      {
+        path: "/user/login",
+        name: "用户登录",
+        component: UserLoginPage,
+      },
+      {
+        path: "/user/register",
+        name: "用户注册",
+        component: UserRegisterPage,
+      },
+    ],
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/admin/user",
+    name: "用户管理",
+    component: AdminUserPage,
+    meta: {
+      access: ACCESS_ENUM.ADMIN,
+    },
+  },
+
   {
     path: "/noAuth",
     name: "无权限",
@@ -34,13 +67,5 @@ export const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/admin",
-    name: "管理页面",
-    component: HomeView,
-    meta: {
-      access: ACCESS_ENUM.ADMIN,
-    },
   },
 ];
