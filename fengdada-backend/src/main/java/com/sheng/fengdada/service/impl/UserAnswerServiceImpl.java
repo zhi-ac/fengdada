a3a60359a52a122df43b6991dec6d4ae5c85a993
zhi-ac <1252580622@qq.com>
2025-01-05 15:06:35 +0800

    添加上幂等性

--- fengdada-backend/src/main/java/com/sheng/fengdada/service/impl/UserAnswerServiceImpl.java
+++ fengdada-backend/src/main/java/com/sheng/fengdada/service/impl/UserAnswerServiceImpl.java
@@ -60,6 +60,8 @@ public class UserAnswerServiceImpl extends ServiceImpl<UserAnswerMapper, UserAns
         if (add) {
             // 补充校验规则
             ThrowUtils.throwIf(appId == null || appId <= 0, ErrorCode.PARAMS_ERROR, "appId 非法");
+            ThrowUtils.throwIf(id == null || id <= 0, ErrorCode.PARAMS_ERROR, "id 不存在");
+
         }
         // 修改数据时，有参数则校验
         // 补充校验规则
